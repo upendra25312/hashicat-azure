9105838885cafd40f7d282453fe01e24fd3a7a5a
Upendra Kumar <upendra25312@gmail.com>
2020-10-14 13:16:57 +0000

    updated marketing slogna

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Coding is the new literacy. 
+  Welcome to ${PREFIX}'s app. Welcome to Hashicorp Terraaform IaC. 
   <!-- END -->
   
   </div>
